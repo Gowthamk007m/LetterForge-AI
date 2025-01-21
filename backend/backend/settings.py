4793d9c558bf736e6b40aab507579a4187923e00
Gowtham K M <gowthamk007m@gmail.com>
2025-01-21 23:28:20 +0530

    settings to production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(j%=h&m@f@4hl!jr@@&karwl*)+hh@yidks9zgp)+1j3d9znlh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
