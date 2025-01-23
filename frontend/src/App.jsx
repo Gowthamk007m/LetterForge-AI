90a78bbb5909867d276138ad3bc66b3274947b6a
Gowtham K M <gowthamk007m@gmail.com>
2025-01-23 12:29:30 +0530

    Removing Facebook Authentication,and login.jsx for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage'
 import Layout from './components/ui/Layout';
 import CoverLetterPage from './pages/CoverLetterPage'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import LandingPage from './pages/LandingPage';
 
@@ -14,11 +12,6 @@ const App = () => {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/cover-letter" element={<CoverLetterPage />} />
-    {/* <GoogleOAuthProvider clientId="137347691807-2ehifvqvrlfqvqagvargud88sg13uuvm.apps.googleusercontent.com">
-    <div className='bg-gray-100 shadow-xl'>
-      <LoginPage />
-    </div>
-    </GoogleOAuthProvider> */}
     </Routes>
     </Layout>
   </Router>
